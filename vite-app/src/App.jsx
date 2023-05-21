eb0349c0895d014ed1ce4a9859c0291c3d221940
tonyfatouros <tony.fatouros@gmail.com>
2023-05-21 23:05:22 +1000

    Modified App.jsx file

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -3,13 +3,13 @@ function App() {
 
   return (
     <>
-      <div class="text-3xl font-bold grid h-screen w-screen place-items-center text-blue-600/30 bg-sky-300">
+      <div className="text-3xl font-bold grid h-screen w-screen place-items-center text-blue-600/30 bg-sky-300">
         Hello
-        <p class="text-blue-600/100">The quick brown fox...</p>
-        <p class="text-blue-600/75">The quick brown fox...</p>
-        <p class="text-blue-600/50">The quick brown fox...</p>
-        <p class="text-blue-600/25">The quick brown fox...</p>
-        <p class="text-blue-600/0">The quick brown fox...</p>
+        <p className="text-blue-600/100">The quick brown fox...</p>
+        <p className="text-blue-600/75">The quick brown fox...</p>
+        <p className="text-blue-600/50">The quick brown fox...</p>
+        <p className="text-blue-600/25">The quick brown fox...</p>
+        <p className="text-blue-600/0">The quick brown fox...</p>
       </div>
     </>
   );
